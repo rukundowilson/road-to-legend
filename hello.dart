fbea60bf7f9a70ed2934f7924057bd839c426913
rukundowilson <rukundowilson5@gmail.com>
2025-07-09 17:26:59 +0200

    dart spell checker

--- hello.dart
+++ hello.dart
@@ -1,5 +1,17 @@
 void main() {
-  helloSomeOne("wilson");
+  // helloSomeOne("wilson");
+  checkAge(13);
+}
+
+void checkAge(int age) {
+  // your code here
+  if (age < 13) {
+    print("you are a child");
+  } else if (age >= 13 && age <= 19) {
+    print("you are a teenager");
+  } else {
+    print("you are an adult");
+  }
 }
 
 void helloSomeOne(String name) {
